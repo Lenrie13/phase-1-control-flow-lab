55d029a5af36fd8a350d5b473157e619ecb5a91b
Lenrie13 <lenrie.ogutu13@gmail.com>
2024-06-29 17:30:35 +0300

    update control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(distance) {
+  let result;
 
-function ternaryCheckCity(){
-  // Write your code here!
+  if (distance <= 400) {
+    result = "This one is on me!";
+  } else if (distance <= 2000) {
+    result = "That will be twenty bucks.";
+  } else if (distance <= 2500) {
+    result = "I will gladly take your thirty bucks.";
+  } else {
+    result = "No can do.";
+  }
+  return result;
 }
+console.log(scuberGreetingForFeet(199));
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function ternaryCheckCity(location) {
+  const isCity = (location === "NYC") ? ("Ok, sounds good.") : ("No go.");
+
+  return isCity;
 }
+console.log(ternaryCheckCity("Pitsburgh"))
+// // Write your code here!
+// Eeeeiiii Mailoooooood!!!!
+
+function switchOnCharmFromTip(clientTip) {
+  
+  let response;
+  switch (clientTip) {
+    case "generous":
+      response = "Thank you so much.";
+      break;
+    case "not as generous":
+      response = "Thank you."
+      break;
+    default:
+      response = "Bye.";
+      break;
+  }
+  return response;
+}
+console.log(switchOnCharmFromTip("generous"));
+
+// // Write your code here!
+
